7ef6d4bcd64ac31cfedc7a6869b8cd578f3cf75d
nanasr <nanasr@sheridancollege.ca>
2020-01-29 19:20:03 -0500

    joker method

--- src/Card.java
+++ src/Card.java
@@ -1,21 +1,39 @@
-public class Card {
-    
-        public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
-        public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-        private final Suit suit;
-        private final Value value;
-        
-        public Card(Suit s, Value gVal)
-        {
-           suit =s;
-           value= gVal;
-        }
-	public Value getValue() {
-		return this.value;
-	}
+/* Auth: Nasreen
+Student _id : 991567814
+ */
 
-	
-	public Suit getSuit() {
-		return this.suit;
-        }
+public class Card {
+    private String joker;
+    private final Suit suit;
+    private final Value value;
+
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    };
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    };
+    
+
+    public String getJoker() {
+        return joker;
+    }
+
+    public void setJoker(String joker) {
+        this.joker = joker;
+    }
+
+    public Card(Suit s, Value gVal) {
+        suit = s;
+        value = gVal;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
 }
